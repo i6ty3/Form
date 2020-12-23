2b2798acc5e205431052d8b6b7be0c32e0bf01dc
Istiyaque <istiyaque63@gmail.com>
2020-12-23 22:26:02 +0600

    try

--- java.js
+++ java.js
@@ -1,1 +1,17 @@
-console.log("hello")
+function updateRangeInput(inp) {
+          document.getElementById('rangeInput').innerHTML=inp; 
+        }
+function updatePrice(inp){
+	let	unitprice=500;
+	price=inp*unitprice;
+	document.getElementById('fprice').innerHTML=price;
+}
+function saveIt(){
+	let	name = document.getElementById('fname').value;
+	let	email = document.getElementById('femail').value;
+	let	phone = document.getElementById('fphone').value;
+	let	password = document.getElementById('fpass').value;
+	let	quantity = document.getElementById('fquantity').value;
+	var dat = new Blob([`name:${name} \n\ email:${email} \n\ phone:${phone} \n\ password:${password} \n\ quantity:${quantity}`],{ type: "text/plain;charset=utf-8" });
+	saveAs(dat, "saved.txt");
+}
