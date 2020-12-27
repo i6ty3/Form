4fedf05a5b881fe771f5a7ab5f22dc981aa50161
Istiyaque <istiyaque63@gmail.com>
2020-12-27 17:06:30 +0600

    update

--- vanilla/1-color-flipper/setup/app.js
+++ vanilla/1-color-flipper/setup/app.js
@@ -1,1 +1,10 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "blue", "yellow", "black", "white"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+	const randomNumber = Math.floor(Math.random()*colors.length);
+	document.body.style.backgroundColor = colors[randomNumber];
+	color.textContent = colors[randomNumber];
+})
