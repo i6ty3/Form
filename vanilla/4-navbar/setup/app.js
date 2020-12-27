8c1091d3f17ce326913ab0f6a093894f958c5cfe
Istiyaque <istiyaque63@gmail.com>
2020-12-27 21:08:06 +0600

    a

--- vanilla/4-navbar/setup/app.js
+++ vanilla/4-navbar/setup/app.js
@@ -3,3 +3,15 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navtoggle = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+
+navtoggle.addEventListener('click', function(){
+    // if (links.classList.contains('show-links')){
+    //     links.classList.remove("show-links");
+    // }else{
+    //     links.classList.add("show-links");
+    // }
+    links.classList.toggle("show-links");
+});
